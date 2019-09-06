f6aaee97f2cb0c686fff6ee327e6fd19539aa401
Guigso <gui.soliveira@hotmail.com>
2019-09-06 16:28:11 -0300

    initial commit

---
+++ src/store/modules/cart/sagas.js
@@ -0,0 +1,13 @@
+import { call, put, all, takeLatest } from 'redux-saga/effects';
+
+import api from '../../../services/api';
+
+import { addToCartSuccess } from './actions';
+
+function* addToCart(action) {
+  const response = yield call(api.get, `/products/${action.id}`);
+
+  yield put(addToCartSuccess(response.data));
+}
+
+export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
